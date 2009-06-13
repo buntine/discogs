b584e60b24937b3eacf080d68c7791e5f711c476
Andy Buntine <info@andrewbuntine.com>
2009-06-13 23:13:57 +1000

    Fleshed out wrapper spec stubs

--- spec/wrapper_spec.rb
+++ spec/wrapper_spec.rb
@@ -12,4 +12,33 @@ describe Discogs::Wrapper do
     @wrapper.api_key.should == @valid_api_key
   end
 
+  describe "when requesting a release" do
+ 
+    before do
+      @valid_release_id = "999"
+      @invalid_release_id = "krematorr"
+    end
+
+    it "should successfully return a Discogs::Release object"
+
+    it "should raise an exception if an invalid API Key is supplied"
+
+    it "should raise an exception if the release does not exist"
+
+    describe "when calling release attributes" do
+
+      before do
+        
+      end
+
+      it "should have a name attribute"
+
+      it "should have an ID attribute"
+
+      it "should have one or more releases"
+
+    end
+
+  end
+
 end
