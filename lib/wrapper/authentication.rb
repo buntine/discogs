4066bf767b8176a6b8928e42e6a51663de51282c
Andy Buntine <bunts@hhd.com.au>
2014-05-20 11:54:27 +1000

    Fixed authenticate method

--- lib/wrapper/authentication.rb
+++ lib/wrapper/authentication.rb
@@ -3,20 +3,22 @@ require "oauth"
 module Authentication
 
   def get_request_token(app_key, app_secret, callback)
-    consumer = OAuth::Consumer.new(app_key, app_secret,
-                 :authorize_url => "http://www.discogs.com/oauth/authorize",
-                 :site          => "http://api.discogs.com")
+    consumer      = OAuth::Consumer.new("nLPklPRYpykkjycrkunw", "fjshPFKjzHUQeFqsjyeWBnmLAnOixRgJ",
+                      :authorize_url => "http://www.discogs.com/oauth/authorize",
+                      :site          => "http://api.discogs.com")
+    request_token = consumer.get_request_token(:oauth_callback => callback)
 
-    {:request_token => consumer.get_request_token(:oauth_callback => callback),
-     :authorize_url => @request_token.authorize_url(:oauth_callback => @callback_url)}
+    {:request_token => request_token,
+     :authorize_url => request_token.authorize_url(:oauth_callback => callback)}
   end
 
   def authenticate(request_token, verifier)
-    @access_token = request_token.get_access_token(:oauth_verifier => verifier)
-    data          = query_and_build_json("oauth/identity")
-    @user         = data.username
+    access_token = request_token.get_access_token(:oauth_verifier => verifier)
+    data         = query_and_build("oauth/identity")
+    user         = data.username
 
-    true
+    {:access_token => access_token,
+     :user         => user}
   end
 
   def authenticated?(username=nil)
