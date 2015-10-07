3264c3a05f255289d73cc2a54264a032e36c87b2
Andrew Buntine von Doom <info@bunts.io>
2015-10-08 10:07:27 +1100

    More test fixes

--- spec/wrapper_methods/get_order_spec.rb
+++ spec/wrapper_methods/get_order_spec.rb
@@ -3,18 +3,25 @@ require 'spec_helper'
 describe Discogs::Wrapper do
 
   before do
-    @oauth_access_token = mock(OAuth::AccessToken)
-    @wrapper = Discogs::Wrapper.new("some_user_agent", @oauth_access_token)
+    @oauth_access_token = double(OAuth::AccessToken)
+    @wrapper = Discogs::Wrapper.new("some_user_agent", :access_token => @oauth_access_token)
+
     @order_id = "1-1"
   end
 
   describe ".get_order" do
 
     before do
-      @oauth_response = mock(OAuth::AccessToken, :code => "200", :body => read_sample("order"))
-      @oauth_response_as_file = mock(StringIO, :read => read_sample("order"))
-      Zlib::GzipReader.should_receive(:new).and_return(@oauth_response_as_file)
-      @oauth_access_token.should_receive(:get).and_return(@oauth_response)
+      @oauth_response = double(OAuth::AccessToken)
+      
+      allow(@oauth_response).to receive_messages(:code => "200", :body => read_sample("order"))
+
+      @oauth_response_as_file = double(StringIO)
+      
+      allow(@oauth_response_as_file).to receive_messages(:read => read_sample("order"))
+
+      expect(Zlib::GzipReader).to receive(:new).and_return(@oauth_response_as_file)
+      expect(@oauth_access_token).to receive(:get).and_return(@oauth_response)
 
       @order = @wrapper.get_order(@order_id)
     end
@@ -22,11 +29,11 @@ describe Discogs::Wrapper do
     describe "when calling simple order attributes" do
 
       it "should have a status" do
-        @order.status.should == "New Order"
+        expect(@order.status).to eq("New Order")
       end
 
       it "should have a fee" do
-        @order.fee.value.should == 2.52
+        expect(@order.fee.value).to eq(2.52)
       end
 
     end
